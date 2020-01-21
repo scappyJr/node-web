98c401a0f91af9149293127b6060610f7c1029ad
김혜원 <terroir7@buyis.co.kr>
2020-01-21 17:32:08 +0900

    - Routing 리팩토링

--- router/index.js
+++ router/index.js
@@ -2,10 +2,15 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = require("path");
+var main = require("./main/main");
+var email = require("./email/email");
 
+// url routing
 router.get("/", function(req, res) {
-  // get 요청일 경우 req.param("email") 이렇게 파라미터 추출 가능
   res.sendFile(path.join(__dirname, "../public/main.html"));
 });
 
-module.exports = router; // router 를 다른 곳으로 export 처리
+router.use("/main", main);
+router.use("/email", email);
+
+module.exports = router;
